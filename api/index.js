cd0214caa64d6acc6ee802fbafb4dc293d3977f6
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2024-11-18 23:01:37 +0530

    Mongo DB Connected

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
-import express from 'express'
+import { log } from "console";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI
+  )
+  .then(() => {
+    console.log("MongoDB is Connected !!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-
 app.listen(3000, () => {
-    console.log('Server running on port 3000 !!!')
-})
+  console.log("Server running on port 3000 !!!");
+});

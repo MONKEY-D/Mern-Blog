95a9a9fcc32d13ba00000938d8026d4f0c8a1fc3
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2024-11-20 03:14:07 +0530

    added Delete functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
-import User from '../models/user.model.js'
+import User from "../models/user.model.js";
 
 export const test = (req, res) => {
   res.json({ message: "API is Working" });
@@ -34,18 +34,34 @@ export const updateUser = async (req, res, next) => {
       );
     }
   }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
-        $set:{ 
-        username: req.body.username,
-        email: req.body.email,
-        profilePicture: req.body.profilePicture,
-        password: req.body.password,
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
       },
-    },{new: true});
-    const {password, ...rest} = updatedUser._doc
-    res.status(200).json(rest)
-    } catch (error) {
-      next(error);
-    }
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been Deleted");
+  } catch (error) {
+    next(error);
+  }
 };

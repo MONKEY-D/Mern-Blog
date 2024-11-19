2a0b0b6bd11c98d761c129e0ec846f519c58d47e
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2024-11-19 21:47:56 +0530

    updated aheader component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Button, Navbar } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 import { TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import {useSelector} from 'react-redux'
 
 export default function Header() {
   const path = useLocation().pathname;
+  const {currentUser} = useSelector(state => state.user)
   return (
     <Navbar className="border-b-2">
       <Link
@@ -32,10 +34,31 @@ export default function Header() {
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
+        {currentUser ? (
+          <Dropdown arrowIcon={false} inline label={
+            <Avatar 
+            alt="user"
+            img={currentUser.profilePicture}
+            rounded
+            />
+          }>
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">@{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+            <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Log Out</Dropdown.Item>
+          </Dropdown>
+        ):(
+          <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue" outline>Login</Button>
         </Link>
-        <Navbar.Toggle/>
+        )
+      }
+      <Navbar.Toggle/>
       </div>
         <Navbar.Collapse>
           <Navbar.Link active={path === '/'} as={'div'}>

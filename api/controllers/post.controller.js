7796c4da39f99bd8cdd3526b530cfa17a8c7c895
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2024-11-22 22:52:35 +0530

    show user post inside dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -33,10 +33,10 @@ export const getposts = async (req, res, next) => {
     const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === "asc" ? 1 : -1;
     const posts = await Post.find({
-      ...(req.query.userId && { userId: req.query.userID }),
-      ...(req.query.category && { userId: req.query.category }),
-      ...(req.query.slug && { userId: req.query.slug }),
-      ...(req.query.postId && { userId: req.query.postId }),
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
       ...(req.query.searchTerm && {
         $or: [
           { title: { $regex: req.query.searchTerm, $options: "i" } },

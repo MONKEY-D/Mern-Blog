43df10f602f82c49871ab75088252c833dba10db
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2024-11-28 22:31:24 +0530

    Complete Post Page Functionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -8,7 +8,7 @@ import {signInSuccess} from '../redux/user/userSlice.js'
 import {useNavigate} from 'react-router-dom'
 
 export default function OAuth() {
-    const auth = getAuth(app)
+    const auth = getAuth(app)             
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const handleGoogleClick = async()=>{

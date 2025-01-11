64a0c8bbf8e15091d7e4e77ae6777233afaaa35b
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2025-01-11 19:57:53 +0530

    fixed the error of tailwind property not rendering

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,32 @@
-import flowbite from "flowbite-react/tailwind";
-import tailwindScrollbar from "tailwind-scrollbar";
-import tailwindLineClamp from "@tailwindcss/line-clamp";
+// import flowbite from "flowbite-react/tailwind";
+// import tailwindScrollbar from "tailwind-scrollbar";
+// import tailwindLineClamp from "@tailwindcss/line-clamp";
 
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{html,js,jsx,ts,tsx}", ...flowbite.content()],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     flowbite.plugin(),
+//     tailwindScrollbar,
+//     tailwindLineClamp,
+//   ],
+// };
+
+
 export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}", ...flowbite.content()],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite.plugin(),
-    tailwindScrollbar,
-    tailwindLineClamp,
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
   ],
 };

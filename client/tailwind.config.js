72948649c3872ad53dbc7eac698847bd46bea17a
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2025-01-11 20:22:45 +0530

    fix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,10 @@
-import flowbite from "flowbite/tailwind";
+import flowbite from "flowbite/plugin";
 import tailwindScrollbar from "tailwind-scrollbar";
 import tailwindLineClamp from "@tailwindcss/line-clamp";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}", ...flowbite.content()],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },

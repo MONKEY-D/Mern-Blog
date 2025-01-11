a90dbbce89b8c183dfd9206c2c353a20c89a3066
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2025-01-11 08:39:34 +0530

    add recent article to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams, useSearchParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -34,28 +36,67 @@ export default function PostPage() {
     fetchPost();
   }, [postSlug]);
 
-  if(loading) return(
-    <div className="flex justify-center min-h-screen">
-        <Spinner size="xl"/>
-    </div>
-  )
-  return <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
-    <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+  useEffect(() => {
+    const fetchRecentPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchRecentPosts(); // Make sure the function is invoked
+  }, []);
+
+  if (loading)
+    return (
+      <div className="flex justify-center min-h-screen">
+        <Spinner size="xl" />
+      </div>
+    );
+  return (
+    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
         {post && post.title}
-        <Link to={`/search?category=${post && post.category}`} className="flex justify-center self-center mt-5">
-        <Button color="gray" pill size="xs">{post && post.category}</Button>
-        </Link>
-        <img src={post && post.image} alt={post && post.title} className="mt-10 p-3 max-h-[600px] w-full object-cover" />
-        <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
-            <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-            <span className="italic">{post && (post.content.length / 1000).toFixed(0)} mins read</span>
+      </h1>
+      <Link
+        to={`/search?category=${post && post.category}`}
+        className="flex justify-center self-center mt-5"
+      >
+        <Button color="gray" pill size="xs">
+          {post && post.category}
+        </Button>
+      </Link>
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className="mt-10 p-3 max-h-[600px] w-full object-cover"
+      />
+      <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span className="italic">
+          {post && (post.content.length / 1000).toFixed(0)} mins read
+        </span>
+      </div>
+      <div
+        className="p-3 max-w-2xl mx-auto w-full post-content"
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
+      </div>
+      <CommentSection postId={post._id} />
+
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Recent Articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
-        <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}}> 
-        </div>
-        <div className="max-w-4xl mx-auto w-full">
-          <CallToAction/>
-        </div>
-        <CommentSection postId={post._id}/>
-    </h1>
-  </main>;
+      </div>
+    </main>
+  );
 }

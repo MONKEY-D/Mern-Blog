a77b69dd1825e13a243fed33ba3e44a03f1118d2
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2025-01-11 21:35:10 +0530

    some known issues

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -132,7 +132,7 @@ export default function CommentSection({ postId }) {
         <div className="text-sm text-teal-500 my-5 flex gap-1">
           You must be signed in to comment.
           <Link className="text-blue-500 hover:underline" to={"/sign-in"}>
-            Sign In
+            Login
           </Link>
         </div>
       )}

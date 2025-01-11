ca0f16ef555deb3e900ef490450cb18fd4488e10
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2025-01-11 20:07:29 +0530

    fixed tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,32 +1,16 @@
-// import flowbite from "flowbite-react/tailwind";
-// import tailwindScrollbar from "tailwind-scrollbar";
-// import tailwindLineClamp from "@tailwindcss/line-clamp";
+import flowbite from "flowbite/tailwind";
+import tailwindScrollbar from "tailwind-scrollbar";
+import tailwindLineClamp from "@tailwindcss/line-clamp";
 
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./src/**/*.{html,js,jsx,ts,tsx}", ...flowbite.content()],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [
-//     flowbite.plugin(),
-//     tailwindScrollbar,
-//     tailwindLineClamp,
-//   ],
-// };
-
-
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", ...flowbite.content()],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
-    require('tailwind-scrollbar'),
+    flowbite.plugin(),
+    tailwindScrollbar,
+    tailwindLineClamp,
   ],
 };

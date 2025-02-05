e3a358f239724d7252a77aeecc53285fa930c13f
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2025-02-05 20:56:05 +0530

    dark mode light mode in sm screen visible now

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -69,12 +69,14 @@ export default function Header() {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill onChange={(e) => setSearchTerm(e.target.value)}>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
         <Button
-          className='w-12 h-10 hidden sm:inline'
+
+          // className='w-12 h-10 hidden sm:inline'
+          className='w-12 h-10'
           color='gray'
           pill
           onClick={() => dispatch(toggleTheme())}
